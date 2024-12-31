566a967e388924f5bfc57d9502ab16634b3c054f
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2024-12-30 19:24:12 -0500

    Decreased the number of "woff"s to increase accuracy so we're not advertising false stuff

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
             />
             <h1>Welcome to Battle Team</h1>
             <p>
-                Battle Team is cool. Here's but a taste of one of our most
-                popular campaign books.
+                Battle Team is cool. Here's but a <b>small</b> taste of one of
+                our most popular campaign books.
             </p>
             <p>
-                <i>...{"woff ".repeat(10000)}...</i>
+                <i>...{"woff ".repeat(500)}...</i>
             </p>
         </div>
     );
