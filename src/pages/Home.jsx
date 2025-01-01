a112171320a1346f3045cfdc915f0b9dd337b246
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2024-12-31 19:47:34 -0500

    Added the button that might make you not get your characters back

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,7 +15,21 @@ const Home = () => {
         >
             <h1>Welcome to Battle Team</h1>
             <p>
-                Battle Team is a rolelplaying game. It's way better than all those other ones. Also, don't worry about any data you had on the old website. It wasn't very important. Also you'll probably be getting it back.{" "}
+                Battle Team is a roleplaying game. It's way better than all
+                those other ones. Also, don't worry about any data you had on
+                the old website. It wasn't very important. Also you'll probably
+                be getting it back. As long as you don't click{" "}
+                <button
+                    onClick={() =>
+                        prompt(
+                            "Hey! Why did you click the button! Now you might now get your characters back...",
+                            "I'm very very very sorry."
+                        )
+                    }
+                >
+                    here
+                </button>
+                .
             </p>
         </div>
     );
