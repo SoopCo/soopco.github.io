0bc8a3bd1025e64395990dc73dbc517105ff933d
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2024-12-31 19:48:33 -0500

    Removing unneeded stuff to make eslint happy :})

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,10 +1,6 @@
 import React from "react";
-import logo from "../images/logo.png";
-import { useNavigate } from "react-router-dom";
 
 const News = () => {
-    const navigate = useNavigate();
-
     return (
         <div
             style={{
