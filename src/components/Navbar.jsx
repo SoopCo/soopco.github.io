3d3c4841ecc2a7882d457f50b3713cd2ed708611
iceninja728 <68028596+iceninja728@users.noreply.github.com>
2025-02-07 09:48:04 -0500

    undid Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,7 +50,7 @@ const Navbar = () => {
                         display: "flex",
                         justifyContent: "center",
                         alignItems: "center",
-                        margin: "0 40px",
+                        margin: "0 10px",
                     }}
                 >
                     <h1>{auth.username}</h1>
