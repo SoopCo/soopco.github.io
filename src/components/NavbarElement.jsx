c2e1a78e5ca465d7a41c4828728c9630fe20e9e1
MatthewWHuang <64226660+MatthewWHuang@users.noreply.github.com>
2025-02-11 15:10:12 -0500

    Added navbar hover effects

--- src/components/NavbarElement.jsx
+++ src/components/NavbarElement.jsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const NavbarElement = ({ to, children, onClick }) => {
+    const [color, setColor] = useState("black");
+
     return (
         <NavLink
             style={{
@@ -10,12 +12,13 @@ const NavbarElement = ({ to, children, onClick }) => {
                 alignItems: "center",
                 margin: "0 10px",
                 textDecoration: "none",
-                color: "black",
-                hover: "blue",
-                active: "light-blue",
+                color: color,
             }}
             to={to}
             onClick={onClick}
+            onMouseEnter={() => setColor("blue")}
+            onMouseLeave={() => setColor("black")}
+            onClickCapture={() => setColor("lightblue")}
         >
             <h1>{children}</h1>
         </NavLink>
