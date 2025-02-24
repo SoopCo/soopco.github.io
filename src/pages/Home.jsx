7291a6117e18e97ed78107f92a0abc6c63471359
iceninja728 <68028596+iceninja728@users.noreply.github.com>
2025-02-23 20:35:31 -0500

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,9 +15,19 @@ const Home = () => {
         >
             <h1>Welcome to Battle Team</h1>
             <p>
-                Battle Team is a roleplaying game. It's way better than all
-                those other ones. Also, don't worry about any data you had on
-                the old website. It wasn't very important.
+                Battle Team is a roleplaying game set in a fantasy environment. All of our campaigns and rulebooks are crafted with love, care, and magic.
+            </p>
+            <p>
+                <bold>
+                    Character of the month: La Gote
+                </bold>
+            </p>
+            <img src="../src/images/gote.png" width="409" height="500">
+            <p>
+                La Gote is a gote (a four-headed hydra goat.) He cofounded the Resistance in an alte- what do you mean this is a spoiler? What do you mean I'm fired?
+            </p>
+            <p>
+                Sorry about that, folks. I just saved you from a spoiler. You're welcome. Anyway, La Gote is a major character in the upcoming campaign "Battle Team: War for Battalia!" Get ready for a cool, awesome, and amazing campaign!
             </p>
         </div>
     );
